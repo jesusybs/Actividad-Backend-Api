d3098405bdc53f5bc8319c31e70bf8cabf8d2723
jesusybs <jesusbanegas.jb8880@gmail.com>
2026-07-21 09:56:52 -0400

    first commit

---
+++ BackendApi/Controllers/ClientesController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc; //Permite utlizar controladores,rutas y respuestas Http
+
+namespace BackendApi.Controllers; 
+
+[ApiController] //Inidica que la clase funcionara como un controlador de una API
+[Route("api/[controller]")] //Define la ruta de una API
+public class ClientesController : ControllerBase // Proporciona funciones de respuesta (Ok(), NotFound(), Badrequest())
+{
+    
+    [HttpGet] //Indica que el metodo respondera a solicitudes GET
+    public IActionResult ObtenerClientes()
+    {
+        var clientes = new[]
+        {
+            new
+            {
+            Id= 1,
+            Nombre = "Juan Perez",
+            Telefono= "77777776"
+            },
+            new
+            {
+            Id= 2,
+            Nombre = "Maria Becerra",
+            Telefono= "77777778"
+            },
+            new
+            {
+            Id= 3,
+            Nombre = "Carlos Martinez",
+            Telefono= "77777779"
+            }
+        };
+        return Ok(clientes);
+        
+    }
+    [HttpGet("{id}")]
+    public IActionResult ObtenerClientePorId(int id)
+    {
+        var cliente = new
+        {
+            Id= id,
+            Nombre = "Cliente Ejemplo",
+            Telefono= "70000000"
+        };
+        return Ok(cliente);
+    }
+    
+}
