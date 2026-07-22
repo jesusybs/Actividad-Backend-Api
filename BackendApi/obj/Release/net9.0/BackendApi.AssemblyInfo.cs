f9874bb5bac8dcdd305567b44e1f6b7b83940976
MeliStarr <desingdaza90@gmail.com>
2026-07-21 23:17:56 -0400

    comit usuario

--- BackendApi/obj/Release/net9.0/BackendApi.AssemblyInfo.cs
+++ BackendApi/obj/Release/net9.0/BackendApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc9d0aec16e343b69dbb91f50ae2a3de038c7be4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf5a8bd6f9864ce9a8db85972aee118a5aff5b4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
